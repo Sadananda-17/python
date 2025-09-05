33c490a1f12b499bd38b18e6a4fb4c455f2cae6d
Sadananda-17 <sadananda17.s.shellikeri@gmail.com>
2025-09-05 20:16:25 +0530

    Add files via upload

---
+++ 4practice.py
@@ -0,0 +1,18 @@
+"""name=input("enter your name: ")
+age=int(input("enter your age: "))
+print("hello"+ name +" good morning "+"you are " + str(age) +" years old")
+print(f"{name} good morning your age {age} years old")
+
+
+sentence=input("enter any sentence:")
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.replace(" ","_"))
+print(sentence.strip())
+
+stringname=input("enter any string:")
+print(len(stringname))
+"""
+
+name="hello \n world\n this is a backslash \\"
+print(name)
