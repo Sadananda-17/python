21d68503f1503dd759cb87762e676614f011c763
Sadananda-17 <sadananda17.s.shellikeri@gmail.com>
2025-09-05 20:14:47 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+print("helloworld")
+print("namaste,nanna hesaru sadananda ss")
+print("python is a very simple language ")
+print("python is a object oriented programiing language")
+print("python is a interpreted language")
+print("python is a high level programming language")
+print("python is a dynamically typed language")
+#python is a very popular language 
+#python is a very simple language as well as powerfull language 
+#python is a versatile language that means we can use python for differnt types fields like web dev ,data science ,data analysis,aiml,etc
+#python is a dynamically typed means we dont need to declare a variable types
+#python is a object oriented programming language
+#its a interpreted language that means we can run line by line but java and c++ are complied languages
+#python is a high level language that means its easy to learn and understand
+#python as a high community support beacuse its as rich library modules and framework for all type of  applications
