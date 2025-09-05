aa52522867a371706dbc248613dc5225240fd8c7
Sadananda-17 <sadananda17.s.shellikeri@gmail.com>
2025-09-05 20:15:45 +0530

    Add files via upload

---
+++ 3UserInput.py
@@ -0,0 +1,32 @@
+#taking user input from user by input 
+"""
+print("take the user input")
+name=input("enter your name ")
+age=input("enter your age ")
+qualification=input("enter your Qualification ")
+par_name=input("enter your parents name")
+sib_name=input("enter your siblings name")
+print(name)
+print(age)
+print(qualification)
+print(par_name)
+print(sib_name)
+"""
+
+
+
+
+boy_name=input("enter boy name: ")
+boy_age=int(input("age"))
+girl_name=input("enter girl name: ")
+girl_age=int(input("age"))
+age_diff=abs(boy_age-girl_age) #absolute it give the value in positive like 30-50=+15 
+print(boy_name+ " loves "+girl_name +"and there differnce is"+ str(age_diff))
+print(f"{boy_name} loves {boy_name} {age_diff}")
+
+#comments in python 
+#single line comment is denoted by #
+""" 
+its a multiline comment
+"""
+
